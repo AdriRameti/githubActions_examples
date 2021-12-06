28c6958e2f4fe34738cdaf7ebce6aa6a103cb240
AdriRameti <adri7agu@gmail.com>
2021-12-06 20:29:32 +0100

    noveno commit

--- app/index.js
+++ app/index.js
@@ -20,7 +20,6 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // messages.
   bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-  
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'Received your message');
   });
